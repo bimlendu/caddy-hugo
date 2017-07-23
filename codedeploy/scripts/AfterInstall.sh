1eea3f29cfd634092cd2b8d01486f2cebece8fbf
Bimlendu Mishra <justbimlendu@gmail.com>
2017-07-24 02:19:17 +0530

    typo

--- codedeploy/scripts/AfterInstall.sh
+++ codedeploy/scripts/AfterInstall.sh
@@ -2,7 +2,7 @@
 
 echo -e "\n\nDeployed at: $(date)" >> /var/www/public/info
 
-echo -e "\n\n* Caddy Plugins *\n-------------------\n" > /var/www/public/info
+echo -e "\n\n* Caddy Plugins *\n-------------------\n" >> /var/www/public/info
 /usr/local/bin/caddy -plugins >> /var/www/public/info
 
 chown www-data:www-data /var/www/public/info
